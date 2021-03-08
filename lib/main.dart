33ddbda4145d90a26e861cce908ea372c0a95173
DindaRizkiHidayah <70737564+DindaRizkiHidayah@users.noreply.github.com>
2021-03-08 21:21:24 +0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,21 +19,38 @@ class MyAppState extends State<MyApp> {
   double _inputUser = 0;
   double _kelvin = 0;
   double _reamur = 0;
+  double _celcius = 0;
+  double _fahrenheit = 0;
   final inputController = TextEditingController();
   String _newValue = "Kelvin";
   double _result = 0;
   List<String> listViewItem = List<String>();
-  void perhitunganCelcius() {
+  void perhitunganSuhu() {
     setState(() {
       _inputUser = double.parse(inputController.text);
-      if (_newValue == "Kelvin") {
-        _result = _inputUser + 273;
+      if (_newValue == "Celcius") {
+        _celcius=_inputUser;
+        _kelvin = _inputUser + 273.15;
+        _reamur =  (4 / 5) *_inputUser ;
+        _fahrenheit =  (9 / 5) *_inputUser + 32;
         listViewItem.add("$_newValue : $_result");
       } else if (_newValue == "Reamur") {
-        _result = (4 / 5) * _inputUser;
+        _reamur=_inputUser;
+        _celcius = _inputUser * (5/4);
+        _fahrenheit = ( 9 / 4) * _inputUser + 32;
+        _kelvin = ( 5 / 4) * _inputUser + 273;
+        listViewItem.add("$_newValue : $_result");
+      } else if (_newValue == "Kelvin") {
+        _celcius = _inputUser - 273;
+        _reamur = (_inputUser - 273) * 4 / 5;
+        _kelvin=_inputUser;
+        _fahrenheit = (_inputUser - 273) * 9 / 5 + 32;
         listViewItem.add("$_newValue : $_result");
       } else {
-        _result = (9 / 5) * _inputUser + 32;
+        _celcius = (_inputUser -32) * 5/9;
+        _reamur = 4/9 * (_inputUser -32);
+        _kelvin = (_inputUser + 469.67) * 5/9;
+        _fahrenheit = _inputUser;
         listViewItem.add("$_newValue : $_result");
       }
     });
@@ -43,10 +60,10 @@ class MyAppState extends State<MyApp> {
     setState(() {
       _inputUser = double.parse(inputController.text);
       if (_newValue == "Kelvin") {
-        _result = (5/4)*_inputUser + 273;
+        _result = (5 / 4) * _inputUser + 273;
         listViewItem.add("$_newValue : $_result");
       } else if (_newValue == "Celcius") {
-        _result = (5/4) * _inputUser;
+        _result = (5 / 4) * _inputUser;
         listViewItem.add("$_newValue : $_result");
       } else {
         _result = (9 / 4) * _inputUser + 32;
@@ -55,38 +72,39 @@ class MyAppState extends State<MyApp> {
     });
   }
 
-  void perhitunganKelvin() {
-    setState(() {
-      _inputUser = double.parse(inputController.text);
-      if (_newValue == "Celcius") {
-        _result = _inputUser - 273;
-        listViewItem.add("$_newValue : $_result");
-      } else if (_newValue == "Reamur") {
-        _result = 4/5 * (_inputUser-273);
-        listViewItem.add("$_newValue : $_result");
-      } else {
-        _result = 9 / 5 * (_inputUser-273) +32;
-        listViewItem.add("$_newValue : $_result");
-      }
-    });
-  }
+  // void perhitunganKelvin() {
+  //   setState(() {
+  //     _inputUser = double.parse(inputController.text);
+  //     if (_newValue == "Celcius") {
+  //       _result = _inputUser - 273;
+  //       listViewItem.add("$_newValue : $_result");
+  //     } else if (_newValue == "Reamur") {
+  //       _result = 4 / 5 * (_inputUser - 273);
+  //       listViewItem.add("$_newValue : $_result");
+  //     } else {
+  //       _result = 9 / 5 * (_inputUser - 273) + 32;
+  //       listViewItem.add("$_newValue : $_result");
+  //     }
+  //   });
+  // }
 
-  void perhitunganFahrenheit() {
-    setState(() {
-      _inputUser = double.parse(inputController.text);
-      if (_newValue == "Celcius") {
-        _result = 5/9 * (_inputUser-32);
-        listViewItem.add("$_newValue : $_result");
-      } else if (_newValue == "Reamur") {
-        _result = 4/9 * (_inputUser-32);
-        listViewItem.add("$_newValue : $_result");
-      } else {
-        _result = 5/9 * (_inputUser-32)+273;
-        listViewItem.add("$_newValue : $_result");
-      }
-    });
-  }
- var listItem1 = ["Celcius", "Kelvin", "Reamur", "Fahrenheit"];
+  // void perhitunganFahrenheit() {
+  //   setState(() {
+  //     _inputUser = double.parse(inputController.text);
+  //     if (_newValue == "Celcius") {
+  //       _result = 5 / 9 * (_inputUser - 32);
+  //       listViewItem.add("$_newValue : $_result");
+  //     } else if (_newValue == "Reamur") {
+  //       _result = 4 / 9 * (_inputUser - 32);
+  //       listViewItem.add("$_newValue : $_result");
+  //     } else {
+  //       _result = 5 / 9 * (_inputUser - 32) + 273;
+  //       listViewItem.add("$_newValue : $_result");
+  //     }
+  //   });
+  // }
+
+  var listItem1 = ["Celcius", "Kelvin", "Reamur", "Fahrenheit"];
   var listItem = ["Celcius", "Kelvin", "Reamur", "Fahrenheit"];
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -119,32 +137,79 @@ class MyAppState extends State<MyApp> {
                         // perhitunganCelcius();
                         // perhitunganKelvin();
                         // perhitunganReamur();
-                        perhitunganFahrenheit();
-                      });
-                    },
-                  ),
-                  DropdownButton<String>(
-                    items: listItem.map((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    value: _newValue,
-                    onChanged: (String changeValue) {
-                      setState(() {
-                        _newValue = changeValue;
-                        // perhitunganCelcius();
-                        // perhitunganKelvin();
-                        // perhitunganReamur();
-                        perhitunganFahrenheit();
+                        // perhitunganFahrenheit();
                       });
                     },
                   ),
                 ],
               ),
               Result(result: _result),
-              Convert(convertHandler: perhitunganCelcius ),
+              Convert(convertHandler: perhitunganSuhu),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: Text(
+                            'Celcius : ',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Container(
+                          child: Text(
+                            'Kelvin : ',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Container(
+                          child: Text(
+                            'Reamur : ',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Container(
+                          child: Text(
+                            'Fahrenheit : ',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: Text(
+                            '$_celcius',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Container(
+                          child: Text(
+                            '$_kelvin',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Container(
+                          child: Text(
+                            '$_reamur',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Container(
+                          child: Text(
+                            '$_fahrenheit',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
               Container(
                 margin: EdgeInsets.only(top: 10, bottom: 10),
                 child: Text(
